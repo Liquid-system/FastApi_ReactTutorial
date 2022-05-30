6dfecd2870c0511e5ec3649caa5bdf84ac3a6efc
Liquid-system <fatum3323@gmail.com>
2022-05-31 02:27:28 +0900

    URLの変更

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import axios from "axios";
 import React from "react";
 
 function App() {
-  const [data, setData] = React.useState();
-  const url = "http://127.0.0.1:8000";
+	const [data, setData] = React.useState();
+	const url = "agile-oasis-32496.herokuapp.com";
 
-  const GetData = () => {
-    axios.get(url).then((res) => {
-      setData(res.data);
-    });
-  };
-  return (
-    <div>
-      <div>ここに処理を書いていきます</div>
-      {data ? (
-        <div>{data.Hello}</div>
-      ) : (
-        <button onClick={GetData} className="button">データを所得</button>
-      )}
-    </div>
-  );
+	const GetData = () => {
+		axios.get(url).then((res) => {
+			setData(res.data);
+		});
+	};
+	return (
+		<div>
+			<div>ここに処理を書いていきます</div>
+			{data ? (
+				<div>{data.Hello}</div>
+			) : (
+				<button onClick={GetData} className="button">データを所得</button>
+			)}
+		</div>
+	);
 }
 
 export default App;
